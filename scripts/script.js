770c42b4ed8fab0831a8a8c5842712aa25d55e99
sm-muhaimen <amuhaimenpro829@gmail.com>
2024-10-08 21:55:12 +0600

    game main feature like player pressed key and expected key pressed done

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,22 @@
+function handleKeyboardKeyUpEvent(event) {
+  //  player pressed key
+  const playerPressed = event.key;
+
+  // expected pressed key
+  const currentAlphabetElement = document.getElementById("current_alphabet");
+  const currentAlphabet = currentAlphabetElement.innerText;
+  const expectedAlphabet = currentAlphabet.toLocaleLowerCase();
+  if (playerPressed === expectedAlphabet) {
+    continueGame();
+    removeBackgroundColor(expectedAlphabet);
+    console.log("you got the point");
+  } else {
+    console.log("XXXXXXXXXXXXXXX");
+  }
+}
+
+document.addEventListener("keyup", handleKeyboardKeyUpEvent);
+
 function continueGame() {
   const alphabet = getARandomAlphabet();
   //   console.log(alphabet);
